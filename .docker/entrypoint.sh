4491604a9e5ae1d0486f17fa33b8e0588e2b2d4e
Lucas Massaharu Hirao <lucas.hirao@fatec.sp.gov.br>
2020-08-14 20:22:44 -0300

    teste

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -3,6 +3,7 @@
 #On error no such file entrypoint.sh, execute in terminal - dos2unix .docker\entrypoint.sh
 touch teste.hmtl
 ls -la
+dockerize -template ./.docker/app/.env:.env -wait tcp://db:3306 -timeout 40s
 chown -R www-data:www-data .
 composer install
 php artisan key:generate
